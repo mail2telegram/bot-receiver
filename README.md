fd99a8506e392e64e9b87dfe764581df47ae04b5
Dmitrii Elkin <7777632@gmail.com>
2020-06-24 16:30:42 +0300

    Readme + example config

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ![Test](https://github.com/mail2telegram/bot-receiver/workflows/Test/badge.svg)
+
+Получает апдейты из Telegram и отправляет их в RabbitMQ.
+Смещение (id следующего апдейта) хранит в Redis. 
+https://core.telegram.org/bots/api#getupdates
--- config.example.php
+++ config.example.php
@@ -7,7 +7,18 @@ use Psr\Log\LoggerInterface;
 return [
     'env' => 'prod', // prod | dev
     'telegramToken' => 'XXX',
-    'telegramLongPollingTimeout' => 5, // 0 for dev
+    'telegramLongPollingTimeout' => 2,
+    'telegramUpdatesLimit' => 100, // 1-100
+    'exchange' => 'telegram_update',
+    'redis' => [
+        'host' => 'm2t_redis',
+    ],
+    'amqp' => [
+        'host' => 'm2t_rabbitmq',
+        'port' => '5672',
+        'user' => 'guest',
+        'pwd' => 'guest',
+    ],
     LoggerInterface::class => static function () {
         $stream = new StreamHandler(STDERR);
         //$stream->setFormatter(new \Dev\CliFormatter());
